0a230fd46c265e4564256cbbf688a5b7e9f6bf77
alexpozdnyakof <alexpozdnyakof@gmail.com>
2025-10-07 21:47:49 +0700

    build(apps/react-app): add typecheck plugin

--- apps/react-app/rsbuild.config.ts
+++ apps/react-app/rsbuild.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginTypeCheck } from "@rsbuild/plugin-type-check";
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [pluginReact(), pluginTypeCheck()],
+  tools: {
+    rspack: {
+      resolve: {
+        pnp: true,
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+      },
+    },
+  },
 });
