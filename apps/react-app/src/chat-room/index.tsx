534fdce9531927157cb5903e50d326a899b850b0
alexpozdnyakof <alexpozdnyakof@gmail.com>
2025-10-27 14:45:33 +0700

    refactor(react-app): integrate message store instead forward socket integration

--- apps/react-app/src/chat-room/index.tsx
+++ apps/react-app/src/chat-room/index.tsx
@@ -1,15 +1,26 @@
 import { UserList } from "./user-list";
 import { MessagesList } from "./messages-list";
-import { users, messages } from "./data";
+import { users } from "./data";
 import { Layout } from "./layout";
 import { Panel } from "./panel";
 import { MessageForm } from "./message-form";
 import type { MessageFormValue } from "./message-form";
+import { useEffect } from "react";
+import { useMessageStore } from "../store/messages.store";
 
 export function ChatRoom() {
+  const { disconnect, messages, send } = useMessageStore();
+
   const addMessage = (formValue: MessageFormValue) => {
-    console.log({ formValue });
+    send({ kind: "request", content: formValue.message });
   };
+
+  useEffect(() => {
+    return () => {
+      disconnect();
+    };
+  }, []);
+
   return (
     <Layout>
       <Panel>
