6cca7ec3367fa9640f67b6620821c3c41c25b5ef
alexpozdnyakof <alexpozdnyakof@gmail.com>
2025-10-29 14:59:28 +0700

    feat(react-app): add message protocol to store

--- apps/react-app/src/store/messages.store.ts
+++ apps/react-app/src/store/messages.store.ts
@@ -1,17 +1,101 @@
 import { create } from "zustand";
 import { WebSocketService } from "../services/websocket.service";
 
-export interface ChatMessage {
-  kind: string;
-  content: string;
+interface User {
+  id: string;
+  name: string;
 }
+
+type IncomeMessage =
+  | {
+    type: "Text";
+    user_id: string;
+    name: string;
+    content: string;
+    timestamp: string;
+  }
+  | { type: "Hello"; users: Array<User>; content: string; timestamp: string }
+  | { type: "Help"; content: string; timestamp: string }
+  | { type: "Joined"; user_id: string; name: string; timestamp: string }
+  | {
+    type: "ChangedName";
+    user_id: string;
+    prev: string;
+    current: string;
+    timestamp: string;
+  }
+  | { type: "Left"; user_id: string; name: string; timestamp: string };
 
 interface Store {
   connected: boolean;
   error: string;
   messages: Array<ChatMessage>;
   disconnect: () => void;
-  send: (message: ChatMessage) => void;
+  send: (message: Pick<ChatMessage, "content">) => void;
+}
+
+export type ChatMessage = {
+  content: string;
+  time: string;
+  user_id: string;
+  name: string;
+};
+
+function userMessage({
+  content,
+  timestamp,
+  name,
+  user_id,
+}: {
+  content: string;
+  timestamp: string;
+  name: string;
+  user_id: string;
+}): ChatMessage {
+  const date = new Date(timestamp);
+  return {
+    user_id,
+    name,
+    content,
+    time: date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }),
+  };
+}
+
+function serverMessage(content: string, utc: string): ChatMessage {
+  const date = new Date(utc);
+  return {
+    user_id: "0",
+    name: "Server",
+    content,
+    time: date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" }),
+  };
+}
+
+function parseMessage(message: IncomeMessage): ChatMessage {
+  switch (message.type) {
+    case "Text":
+      return userMessage(message);
+    case "Hello":
+      return serverMessage(message.content, message.timestamp);
+
+    case "Help":
+      return serverMessage(message.content, message.timestamp);
+    case "Joined": {
+      const text = `${message.name} вошёл в комнату`;
+      return serverMessage(text, message.timestamp);
+    }
+    case "ChangedName": {
+      const text = `${message.prev} теперь ${message.current}`;
+      return serverMessage(text, message.timestamp);
+    }
+    case "Left": {
+      const text = `${message.name} покинул  комнату`;
+      return serverMessage(text, message.timestamp);
+    }
+    default: {
+      throw new Error("Unknown message type");
+    }
+  }
 }
 
 const service = WebSocketService(
@@ -26,11 +110,11 @@ export const useMessageStore = create<Store>((set) => {
     }),
   );
 
-  service.eventHandler("message", (message) =>
+  service.eventHandler("message", (message) => {
     set((state) => ({
-      messages: [...state.messages, JSON.parse(message)],
-    })),
-  );
+      messages: [...state.messages, parseMessage(JSON.parse(message))],
+    }));
+  });
 
   service.eventHandler("close", () => set({ connected: false }));
   service.eventHandler("error", (error) => set({ error: error.message }));
@@ -40,7 +124,7 @@ export const useMessageStore = create<Store>((set) => {
     connected: false,
     error: "",
     messages: [],
-    send: (message: ChatMessage) => {
+    send: (message: Pick<ChatMessage, "content">) => {
       service.send(JSON.stringify(message));
     },
     disconnect: () => {
